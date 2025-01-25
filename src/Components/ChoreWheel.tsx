bd2d5b1740bbcdffb331de6b656d6c0cce7fc8fc
Austin Stubbs <austin.emuangstubbs@gmail.com>
2025-01-25 15:54:20 -0600

    Add TailwindCSS integration and refactor styles for Roomies app

--- src/Components/ChoreWheel.tsx
+++ src/Components/ChoreWheel.tsx
@@ -4,9 +4,10 @@ function ChoreWheel() {
 
     return (
         <>
-            <div id="ChoreWheel">
+            <div className="p-2 m-4 h-8/10 w-2/3 border-white rounded-[50px]
+             border-5" id="ChoreWheel">
 
-            <h1>Chore Wheel</h1>
+            <h1 className="font-sans text-sky-900">Chore Wheel</h1>
 
             </div>
         </>
--- src/Components/Household.tsx
+++ src/Components/Household.tsx
@@ -4,10 +4,10 @@ function Household() {
 
     return (
         <>
-            <div id="Household">
-                <h1>Your Household</h1>
-                <input value="Roomie Name..."></input>
-                <button>Add Roomie</button>
+            <div className="p-2 m-4 h-fit" id="Household">
+                <h1 className="font-sans text-sky-900">Your Household</h1>
+                <input className="font-sans text-sky-900" value="Roomie Name..."></input>
+                <button className="font-sans py-2 px-4  text-white shadow-2xl bg-fuchsia-300 hover:bg-fuchsia-400 border-white rounded-[50px]">Add Roomie</button>
             </div>
         </>
     )
