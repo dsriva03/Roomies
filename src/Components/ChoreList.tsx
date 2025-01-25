8810af8b3cddd1edca4b13b094032ac9a5a7d30f
Jeremy Dalton <jeremydalton91@gmail.com>
2025-01-25 13:07:14 -0500

    Basic react components added

---
+++ src/Components/ChoreList.tsx
@@ -0,0 +1,16 @@
+
+function ChoreList() {
+
+    return (
+        <>
+            <div id="ChoreList">
+                <h1>Chore List</h1>
+                <input value="Chore..."></input>
+                <button>Add Chore</button>
+            </div>
+        </>
+
+    )
+}
+
+export default ChoreList
---
+++ src/Components/ChoreWheel.tsx
@@ -0,0 +1,16 @@
+
+
+function ChoreWheel() {
+
+    return (
+        <>
+            <div id="ChoreWheel">
+
+            <h1>Chore Wheel</h1>
+
+            </div>
+        </>
+    )
+}
+
+export default ChoreWheel
