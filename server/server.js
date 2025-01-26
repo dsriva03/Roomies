955203f8b7b5444dd32ae6de24a9fcb40bdbd96d
dsriva03 <dsriva03@gmail.com>
2025-01-25 20:05:35 -0500

    server side middlewares and routers

---
+++ server/server.js
@@ -0,0 +1,75 @@
+//* require and import dependencies
+//require the path 
+const path = require("path");
+
+//require express
+const express = require("express");
+
+//create an app instance of express
+const app = express();
+
+//import controllers
+const roomiesController = require('./controllers/roomiesController')
+
+const apiRouter = require('./routes/api'); //importing RESTful routes from routes folder
+
+const PORT = 8080; //defining port as 8080, same as Vite
+
+//* global middleware functions
+
+//cors acceptance middleware???
+    //?check if need to install cors, don't think so because react and express are running on the same port 3000??
+    //! install CORS, make sure its saved
+
+//json parser middleware
+app.use(express.json()); //deciphers text into readable json body
+
+//urlencoded middleware
+app.use(express.urlencoded()); //recognizes incoming request object into readable strings and arrays
+
+//^^^^ NEED FOR POST AND PUT REQUESTS //-https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded
+
+//serving static files requests for client middleware //? MODIFY PATH LATER, is it App?
+    //?serves React build app (when running npm run build)??? ASK 
+app.use(express.static(path.resolve(__dirname, '../src/assets'))) //!DOUBLE CHECK!!!
+
+
+app.use('/api', apiRouter);
+
+//* logging HTTP method and the URL of the request for debugging 
+    //logs the INCOMING request before it reaches any routes, so it needs to be before routes middleware
+app.use((req, res, next) => {
+    console.log(`${req.method}`, `${req.url}`)
+    next(); //pass it over to the next function
+});
+
+//* Express moves onto route
+
+//! add catch all error handler for incorrect routes
+app.use((req, res) => res.status(404).send('This is not the page you\re looking for.'))
+
+
+//* global error handler 
+//create global error handler + Object.assign the error
+app.use((err, req, res, _next) => {
+    //create a default error to have an error to fall back on
+    const defaultError = {
+        log: 'Express caught an unknown middleware error.',
+        status: 500, 
+        message: {err: 'An error occured.'}
+    };
+
+    const errorObject = Object.assign({}, defaultError, err);
+    console.log(errorObject.log); //log it for developer use, because returning it like status and message can reveal sensitive info
+    return res.status(errorObject.status).json(errorObject.message)
+
+});
+
+
+
+//*create port for app to listen - 
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+});
+
+//server.js: single source of truth: express follows the order of this page ONLY, so import other controllers and middlewares here and put them after the global middleware
